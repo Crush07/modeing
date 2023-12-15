47988fc12c2a3ed3ffe9365baf61c6c14155883c
hyy <huangyiyang@crownposci.com>
2023-12-15 23:47:36 +0800

    feat: 坐标轴原点坐标为double类型

--- src/main/java/com/hysea/entity/CoordinateSystem.java
+++ src/main/java/com/hysea/entity/CoordinateSystem.java
@@ -2,15 +2,51 @@ package com.hysea.entity;
 
 public class CoordinateSystem {
 
-    private int x;
+    private double x;
 
-    private int y;
+    private double y;
 
-    private int z;
+    private double z;
 
-    public CoordinateSystem(int x, int y, int z) {
+    //坐标轴长度
+    private double length;
+
+    public CoordinateSystem(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+        length = 500;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
     }
 }
