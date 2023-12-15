ca97ed9d28f94c7b747a050fe202c5938abf6abc
hyy <huangyiyang@crownposci.com>
2023-12-15 17:29:54 +0800

    fix: 分数类，计算公约数改用迭代方式防止内存溢出，计算产生新对象，不改对象本身

--- src/main/java/com/hysea/core/Fraction.java
+++ src/main/java/com/hysea/core/Fraction.java
@@ -42,37 +42,32 @@ public class Fraction {
                 '}';
     }
 
-    //乘法
-    public Fraction multiply(double d){
-        this.numerator *= d;
-        return this;
+    // 乘法
+    public Fraction multiply(double d) {
+        return new Fraction(this.numerator * d, this.denominator).simplify();
     }
 
-    public Fraction multiply(Fraction f){
-        this.denominator *= f.denominator;
-        this.numerator *= f.numerator;
-        return this;
+    public Fraction multiply(Fraction f) {
+        return new Fraction(this.numerator * f.numerator, this.denominator * f.denominator).simplify();
     }
 
-    //除法
-    public Fraction divide(double d){
-        this.denominator *= d;
-        return this;
+    // 除法
+    public Fraction divide(double d) {
+        return new Fraction(this.numerator, this.denominator * d).simplify();
     }
 
-    public Fraction divide(Fraction f){
-        this.denominator *= f.numerator;
-        this.numerator *= f.denominator;
-        return this;
+    public Fraction divide(Fraction f) {
+        return new Fraction(this.numerator * f.denominator, this.denominator * f.numerator).simplify();
     }
 
-    //加法
+    // 加法
     public Fraction add(Fraction f) {
         double commonDenominator = findCommonDenominator(this.denominator, f.denominator);
-        this.numerator = (this.numerator * (commonDenominator / this.denominator)) +
-                (f.numerator * (commonDenominator / f.denominator));
-        this.denominator = commonDenominator;
-        return this;
+        return new Fraction(
+                (this.numerator * (commonDenominator / this.denominator)) +
+                        (f.numerator * (commonDenominator / f.denominator)),
+                commonDenominator
+        ).simplify();
     }
 
     public Fraction add(double d) {
@@ -80,18 +75,31 @@ public class Fraction {
         return this.add(fractionD);
     }
 
-    //减法
+    // 减法
     public Fraction subtract(Fraction f) {
         double commonDenominator = findCommonDenominator(this.denominator, f.denominator);
-        this.numerator = (this.numerator * (commonDenominator / this.denominator)) -
-                (f.numerator * (commonDenominator / f.denominator));
-        this.denominator = commonDenominator;
-        return this;
+        return new Fraction(
+                (this.numerator * (commonDenominator / this.denominator)) -
+                        (f.numerator * (commonDenominator / f.denominator)),
+                commonDenominator
+        ).simplify();
     }
 
     public Fraction subtract(double d) {
         Fraction fractionD = new Fraction(d);
         return this.subtract(fractionD);
+    }
+
+    // 化简分数
+    public Fraction simplify() {
+        double gcd = findGCD(this.numerator, this.denominator);
+
+        if (gcd != 0) {
+            this.numerator /= gcd;
+            this.denominator /= gcd;
+        }
+
+        return this;
     }
 
     // 寻找两个数的最小公倍数
@@ -100,12 +108,14 @@ public class Fraction {
     }
 
     // 寻找两个数的最大公约数
-    private double findGCD(double a, double b) {
-        if (b == 0) {
-            return a;
-        } else {
-            return findGCD(b, a % b);
+    public static double findGCD(double a, double b) {
+        // 使用欧几里得算法
+        while (b != 0) {
+            double temp = b;
+            b = a % b;
+            a = temp;
         }
+        return Math.abs(a); // 返回最大公约数，取绝对值确保结果是非负数
     }
 
     public double getValue(){
