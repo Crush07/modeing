9da1d093bf0190e424965492784c747a554fc83b
hyy <huangyiyang@crownposci.com>
2023-12-25 23:25:06 +0800

    fix: 初始化线条，根据鼠标移动事件获取接近的直线，并将直线改变颜色

--- src/main/java/com/hysea/canvas/CanvasPanel.java
+++ src/main/java/com/hysea/canvas/CanvasPanel.java
@@ -18,10 +18,14 @@ public class CanvasPanel extends JPanel {
     private Viewpoint viewpoint;
     private CoordinateSystem coordinateSystem;
 
+    Line[] lineArray = new Line[120];
+
     public CanvasPanel() {
         // 初始化画布，设置视点等
         viewpoint = new Viewpoint();
         coordinateSystem = new CoordinateSystem(0,0,0);
+
+        drawCoordinateSystem();
 
         //MouseMotionAdapter的mouseDragged监听事件才管用
         MouseMotionAdapter mouseMotionAdapter = new MouseMotionAdapter() {
@@ -96,9 +100,19 @@ public class CanvasPanel extends JPanel {
                 repaint();
             }
 
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                Point point = new Point(new Fraction(e.getX()), new Fraction(e.getY()));
+                selectLine(point,lineArray);
+                repaint();
+            }
         };
 
         this.addMouseMotionListener(mouseMotionAdapter);
+
+//        this.addMouseListener(new MouseAdapter(){
+//
+//        });
 
     }
 
@@ -112,10 +126,30 @@ public class CanvasPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        drawCoordinateSystem(g);
+        g.setColor(new Color(29,55,56));
+        g.fillRect(0,0,getWidth(),getHeight());
+
+        // 白色画笔
+        g.setColor(Color.BLACK);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        // 启用抗锯齿
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        for (Line line : lineArray) {
+            System.out.println(line);
+            drawLine(g2d,line);
+        }
     }
 
     public void drawLine(Graphics2D g,Line line){
+
+        Color originColor = g.getColor();
+        if(line.isBeSelected()){
+            g.setColor(Color.WHITE);
+        }
+
         // 获取面板的中心点坐标
         int minX = 0,minY = 0;
         int maxX = getWidth();
@@ -145,162 +179,43 @@ public class CanvasPanel extends JPanel {
         Line l = new Line(new Point[]{top1Point,top2Point});
         l.draw(g);
 
+        g.setColor(originColor);
     }
 
-    public void drawCoordinateSystem(Graphics g){
-//        // 获取面板的中心点坐标
-//        int minX = 0,minY = 0;
-//        int maxX = getWidth();
-//        int maxY = getHeight();
-//        int centerX = (maxX + minY) / 2;
-//        int centerY = (maxY + minY) / 2;
-//
-//        Point topX = new Point(coordinateSystem.getX() + coordinateSystem.getLength(), 0, 0);
-//        Point topY = new Point(0,coordinateSystem.getY() + coordinateSystem.getLength(), 0);
-//        Point topZ = new Point(0,0,coordinateSystem.getZ() + coordinateSystem.getLength());
-//        Point topOrigin = new Point(0,0,0);
-//
-//        //视平面
-//        Face faceByNormalVectorAndPoint = Calculator.getFaceByNormalVectorAndPoint(viewpoint.getVector(), viewpoint.getPoint());
-//
-//        //穿过topX与视线向量平行的直线
-//        Line lineByVectorAndPointX = Calculator.getLineByVectorAndPoint(viewpoint.getVector(), topX);
-//        System.out.println(lineByVectorAndPointX);
-//        //穿过topY与视线向量平行的直线
-//        Line lineByVectorAndPointY = Calculator.getLineByVectorAndPoint(viewpoint.getVector(), topY);
-//        System.out.println(lineByVectorAndPointY);
-//        //穿过topZ与视线向量平行的直线
-//        Line lineByVectorAndPointZ = Calculator.getLineByVectorAndPoint(viewpoint.getVector(), topZ);
-//        System.out.println(lineByVectorAndPointZ);
-//        //穿过topO与视线向量平行的直线
-//        Line lineByVectorAndPointOrigin = Calculator.getLineByVectorAndPoint(viewpoint.getVector(), topOrigin);
-//        System.out.println(lineByVectorAndPointOrigin);
-//
-//        //投影到视平面上的topX
-//        Point pointByParametricEquationOfLineAndCommonEquationOfFaceX = Calculator.getPointByParametricEquationOfLineAndCommonEquationOfFace(lineByVectorAndPointX, faceByNormalVectorAndPoint);
-//        //投影到视平面上的topY
-//        Point pointByParametricEquationOfLineAndCommonEquationOfFaceY = Calculator.getPointByParametricEquationOfLineAndCommonEquationOfFace(lineByVectorAndPointY, faceByNormalVectorAndPoint);
-//        //投影到视平面上的topZ
-//        Point pointByParametricEquationOfLineAndCommonEquationOfFaceZ = Calculator.getPointByParametricEquationOfLineAndCommonEquationOfFace(lineByVectorAndPointZ, faceByNormalVectorAndPoint);
-//        //投影到视平面上的topO
-//        Point pointByParametricEquationOfLineAndCommonEquationOfFaceOrigin = Calculator.getPointByParametricEquationOfLineAndCommonEquationOfFace(lineByVectorAndPointOrigin, faceByNormalVectorAndPoint);
-//
-//        //转换成视平面上的直角坐标系坐标
-//        Point mapX = Calculator.getPointInRectangularCoordinateSystemByVectorXAndVectorYAndPointToOriginPoint(viewpoint.getVectorX(), viewpoint.getVectorY(), pointByParametricEquationOfLineAndCommonEquationOfFaceX, viewpoint.getPoint());
-//        Point mapY = Calculator.getPointInRectangularCoordinateSystemByVectorXAndVectorYAndPointToOriginPoint(viewpoint.getVectorX(), viewpoint.getVectorY(), pointByParametricEquationOfLineAndCommonEquationOfFaceY, viewpoint.getPoint());
-//        Point mapZ = Calculator.getPointInRectangularCoordinateSystemByVectorXAndVectorYAndPointToOriginPoint(viewpoint.getVectorX(), viewpoint.getVectorY(), pointByParametricEquationOfLineAndCommonEquationOfFaceZ, viewpoint.getPoint());
-//        Point mapOrigin = Calculator.getPointInRectangularCoordinateSystemByVectorXAndVectorYAndPointToOriginPoint(viewpoint.getVectorX(), viewpoint.getVectorY(), pointByParametricEquationOfLineAndCommonEquationOfFaceOrigin, viewpoint.getPoint());
-//
-//        //映射到视平面的原点直角坐标系坐标
-//        int mapCenterX = centerX + (int)mapOrigin.getX().getValue();
-//        int mapCenterY = centerY + (int)mapOrigin.getY().getValue();
+    public boolean isSelected(Point mousePoint, Line line){
+        // 获取面板的中心点坐标
+        int minX = 0,minY = 0;
+        int maxX = getWidth();
+        int maxY = getHeight();
+        int centerX = (maxX + minY) / 2;
+        int centerY = (maxY + minY) / 2;
 
-        g.setColor(new Color(29,55,56));
-        g.fillRect(0,0,getWidth(),getHeight());
+        //视平面
+        Face faceByNormalVectorAndPoint = Calculator.getFaceByNormalVectorAndPoint(viewpoint.getVector(), viewpoint.getPoint());
 
-        // 白色画笔
-        g.setColor(Color.WHITE);
+        //穿过topX与视线向量平行的直线
+        Line lineByVectorAndPoint1 = Calculator.getLineByVectorAndPoint(viewpoint.getVector(), line.getPoints()[0]);
+        //穿过topY与视线向量平行的直线
+        Line lineByVectorAndPoint2 = Calculator.getLineByVectorAndPoint(viewpoint.getVector(), line.getPoints()[1]);
 
-        Graphics2D g2d = (Graphics2D) g;
+        //投影到视平面上的topX
+        Point pointByParametricEquationOfLineAndCommonEquationOfFace1 = Calculator.getPointByParametricEquationOfLineAndCommonEquationOfFace(lineByVectorAndPoint1, faceByNormalVectorAndPoint);
+        //投影到视平面上的topY
+        Point pointByParametricEquationOfLineAndCommonEquationOfFace2 = Calculator.getPointByParametricEquationOfLineAndCommonEquationOfFace(lineByVectorAndPoint2, faceByNormalVectorAndPoint);
 
-        // 启用抗锯齿
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //转换成视平面上的直角坐标系坐标
+        Point map1 = Calculator.getPointInRectangularCoordinateSystemByVectorXAndVectorYAndPointToOriginPoint(viewpoint.getVectorX(), viewpoint.getVectorY(), pointByParametricEquationOfLineAndCommonEquationOfFace1, viewpoint.getPoint());
+        Point map2 = Calculator.getPointInRectangularCoordinateSystemByVectorXAndVectorYAndPointToOriginPoint(viewpoint.getVectorX(), viewpoint.getVectorY(), pointByParametricEquationOfLineAndCommonEquationOfFace2, viewpoint.getPoint());
 
-//
-//        Point originPoint = new Point(mapCenterX, mapCenterY);
-//
-//        Line topLine = new Line(new Point[]{
-//                new Point(new Fraction(minX),new Fraction(minY)),
-//                new Point(new Fraction(maxX),new Fraction(minY))
-//        });
-//
-//        Line leftLine = new Line(new Point[]{
-//                new Point(new Fraction(minX),new Fraction(minY)),
-//                new Point(new Fraction(minX),new Fraction(maxY))
-//        });
-//
-//        Line bottomLine = new Line(new Point[]{
-//                new Point(new Fraction(minX),new Fraction(maxY)),
-//                new Point(new Fraction(maxX),new Fraction(maxY))
-//        });
-//
-//        Line rightLine = new Line(new Point[]{
-//                new Point(new Fraction(maxX),new Fraction(minY)),
-//                new Point(new Fraction(maxX),new Fraction(maxY))
-//        });
-//
-//
-//        Point topXPoint = new Point(centerX + (int)mapX.getX().getValue(),centerY + (int)mapX.getY().getValue());
-//        Line xAxle = new Line(new Point[]{originPoint,topXPoint});
-//        Point xAxlePoint = Calculator.getPointByParametricEquationOfLine1AndSlopeExpressionOfLine2(xAxle, topLine);
-//        if(xAxlePoint == null){
-//            xAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(xAxle, leftLine);
-//        }
-//        if(xAxlePoint == null){
-//            xAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(xAxle, bottomLine);
-//        }
-//        if(xAxlePoint == null){
-//            xAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(xAxle, rightLine);
-//        }
-//        System.out.println("xAxlePoint"+xAxlePoint);
-//        if(xAxlePoint != null){
-//            xAxle.getPoints()[1] = xAxlePoint;
-//        }
-//        xAxle.draw(g);
-//
-//        Point topYPoint = new Point(centerX + (int)mapY.getX().getValue(),centerY + (int)mapY.getY().getValue());
-//        Line yAxle = new Line(new Point[]{originPoint,topYPoint});
-//        Point yAxlePoint = Calculator.getPointByParametricEquationOfLine1AndSlopeExpressionOfLine2(yAxle, topLine);
-//        if(yAxlePoint == null){
-//            yAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(yAxle, leftLine);
-//        }
-//        if(yAxlePoint == null){
-//            yAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(yAxle, bottomLine);
-//        }
-//        if(yAxlePoint == null){
-//            yAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(yAxle, rightLine);
-//        }
-//        System.out.println("yAxlePoint"+yAxlePoint);
-//        if(yAxlePoint != null){
-//            yAxle.getPoints()[1] = yAxlePoint;
-//        }
-//        yAxle.draw(g);
-//
-//        Point topZPoint = new Point(centerX + (int)mapZ.getX().getValue(),centerY + (int)mapZ.getY().getValue());
-//        Line zAxle = new Line(new Point[]{originPoint,topZPoint});
-//        Point zAxlePoint = Calculator.getPointByParametricEquationOfLine1AndSlopeExpressionOfLine2(zAxle, topLine);
-//        if(zAxlePoint == null){
-//            zAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(zAxle, leftLine);
-//        }
-//        if(zAxlePoint == null){
-//            zAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(zAxle, bottomLine);
-//        }
-//        if(zAxlePoint == null){
-//            zAxlePoint = Calculator.getPointByParametricEquationOfLine1AndCommonEquationOfLine2(zAxle, rightLine);
-//        }
-//        System.out.println("zAxlePoint"+zAxlePoint);
-//        if(zAxlePoint != null){
-//            zAxle.getPoints()[1] = zAxlePoint;
-//        }
-//        zAxle.draw(g);
-//        Line[] lineArray = {
-//                new Line(new Point[]{new Point(-50, -50, -50), new Point(50, -50, -50)}),
-//                new Line(new Point[]{new Point(50, -50, -50), new Point(50, 50, -50)}),
-//                new Line(new Point[]{new Point(50, 50, -50), new Point(-50, 50, -50)}),
-//                new Line(new Point[]{new Point(-50, 50, -50), new Point(-50, -50, -50)}),
-//
-//                new Line(new Point[]{new Point(-50, -50, 50), new Point(50, -50, 50)}),
-//                new Line(new Point[]{new Point(50, -50, 50), new Point(50, 50, 50)}),
-//                new Line(new Point[]{new Point(50, 50, 50), new Point(-50, 50, 50)}),
-//                new Line(new Point[]{new Point(-50, 50, 50), new Point(-50, -50, 50)}),
-//
-//                new Line(new Point[]{new Point(-50, -50, -50), new Point(-50, -50, 50)}),
-//                new Line(new Point[]{new Point(50, -50, -50), new Point(50, -50, 50)}),
-//                new Line(new Point[]{new Point(50, 50, -50), new Point(50, 50, 50)}),
-//                new Line(new Point[]{new Point(-50, 50, -50), new Point(-50, 50, 50)})
-//        };
+        Point top1Point = new Point(centerX + (int)map1.getX().getValue(),centerY + (int)map1.getY().getValue());
+        Point top2Point = new Point(centerX + (int)map2.getX().getValue(),centerY + (int)map2.getY().getValue());
+        Line l = new Line(new Point[]{top1Point,top2Point});
 
-        Line[] lineArray = new Line[120];
+        return Calculator.isPoint2DInLine2D(mousePoint,l);
+
+    }
+
+    public void drawCoordinateSystem(){
 
         double radius = 100;
 
@@ -343,11 +258,20 @@ public class CanvasPanel extends JPanel {
 
             lineArray[i + 96] = line2;
         }
+    }
 
 
-
+    public void selectLine(Point mousePoint, Line[] lineArray){
         for (Line line : lineArray) {
-            drawLine(g2d,line);
+            if(isSelected(mousePoint,line)){
+                line.setBeSelected(true);
+            }else{
+                line.setBeSelected(false);
+            }
         }
+    }
+
+    public void drawFutureLine(Graphics g){
+
     }
 }
