40bb8f55d53f4ed2bf76786ecb9f67144cdfc038
hyy <huangyiyang@crownposci.com>
2023-12-25 23:26:48 +0800

    feat: 可被选择父类，直线继承该父类，表示在直线可以被选中

---
+++ src/main/java/com/hysea/entity/CanBeSelectedObject.java
@@ -0,0 +1,14 @@
+package com.hysea.entity;
+
+public class CanBeSelectedObject {
+
+    boolean isBeSelected;
+
+    public boolean isBeSelected() {
+        return isBeSelected;
+    }
+
+    public void setBeSelected(boolean beSelected) {
+        isBeSelected = beSelected;
+    }
+}
